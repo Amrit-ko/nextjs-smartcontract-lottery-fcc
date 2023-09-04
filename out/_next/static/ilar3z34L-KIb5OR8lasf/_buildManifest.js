aa7de30efd96854cfaa415d1143676dd5f99a8bb
Kostyantin Osadchii <99597545+Amrit-ko@users.noreply.github.com>
2023-09-04 18:00:32 +0300

    Delete out directory

--- out/_next/static/ilar3z34L-KIb5OR8lasf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/541-366d4caec1e49db4.js","static/css/39f4d8aba4b66723.css","static/chunks/pages/index-4fcc0a9d37fe2065.js"],"/_error":["static/chunks/pages/_error-0509152792d2b207.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
